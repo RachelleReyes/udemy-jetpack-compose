d795a93cc51ad4655889b921140e20dbb472ea50
Rachelle Reyes <reyes.rachelle@uabc.edu.mx>
2022-11-20 12:18:14 -0800

    Continuation Jetpack Compose fundamentals

--- IntroToCompose/app/src/main/java/com/reyesrachelle/introtocompose/MainActivity.kt
+++ IntroToCompose/app/src/main/java/com/reyesrachelle/introtocompose/MainActivity.kt
@@ -3,14 +3,21 @@ package com.reyesrachelle.introtocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.reyesrachelle.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,37 +26,72 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntroToComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxHeight()
-                        .fillMaxWidth()
-                        .padding(all = 59.dp),
-                    color = MaterialTheme.colors.primary
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    val moneyCounter = remember {
+        mutableStateOf(0)
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "$${moneyCounter.value}", style = TextStyle(
+                color = Color.White,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.ExtraBold
+            ))
+            Spacer(modifier = Modifier.height(130.dp))
+            CreateCircle(moneyCounter = moneyCounter.value) { newValue ->
+                moneyCounter.value = newValue
+            }
+
+            if(moneyCounter.value > 25) {
+                Text("Lots of money!")
+            }
+        }
+    }
 }
 
-@Preview
+//@Preview
 @Composable
-fun ShowAge(age: Int = 12) {
-    Text(text = age.toString())
+fun CreateCircle(moneyCounter: Int = 0,
+                 updatedMoneyCounter: (Int) -> Unit) {
+//    var moneyCounter by remember {
+//        mutableStateOf(0)
+//    }
+
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(105.dp)
+        .clickable {
+            updatedMoneyCounter(moneyCounter + 1)
+                   },
+        shape = CircleShape,
+        elevation = 4.dp
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap", modifier = Modifier)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntroToComposeTheme {
-        Column() {
-            Greeting("Android")
-            ShowAge(age = 34)
-        }
+        MyApp()
     }
 }
